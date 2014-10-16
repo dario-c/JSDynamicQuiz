cd6dcab5a783c7c79ac5ff8b7aacec2b8637661f
dario-c <dariocavanillas@gmail.com>
2014-10-16 22:34:32 +0200

    Created a start button that displays 1st question on.click

--- quiz.js
+++ quiz.js
@@ -1,6 +1,3 @@
-/**
- * Created by dario on 16/10/14.
- */
 var questions = [
     {
     question: "Who is the Prime Minister of Germany?",
@@ -17,3 +14,46 @@ var questions = [
     }
 ];
 
+var startButton = document.getElementById('start');
+
+startButton.addEventListener("click", function(){
+    var parent = this.parentNode;
+    parent.removeChild(this);
+
+    parent.appendChild( composeQuestion() );
+}, false);
+
+
+function composeQuestion(){
+    var questionBox = document.createElement('div');
+    questionBox.id = 'question';
+    insertTitleInto(questionBox);
+    insertChoicesInto(questionBox);
+    return questionBox;
+}
+
+function insertTitleInto(parent) {
+    var questionTitle = document.createElement("h2");
+    var textNode = document.createTextNode(questions[0].question);
+    questionTitle.appendChild(textNode);
+    parent.appendChild(questionTitle);
+}
+
+function insertChoicesInto(parent){
+    var all = questions[0].choices;
+    for(var i = 0; i < all.length; i++){
+        parent.appendChild(createEachChoice(all[i], i));
+
+        parent.appendChild(document.createTextNode(all[i]));
+        parent.appendChild(document.createElement("br"));
+    }
+}
+
+function createEachChoice(choice, i){
+    var newNode = document.createElement("input");
+    newNode.type = "radio";
+    newNode.name = "choices";
+    newNode.value = i;
+
+    return newNode;
+}
