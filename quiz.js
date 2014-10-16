e3d61bbf93358081db6465b1c647d39bc5f66711
dario-c <dariocavanillas@gmail.com>
2014-10-16 15:53:56 +0200

    added 3 questions

--- quiz.js
+++ quiz.js
@@ -1,3 +1,19 @@
 /**
  * Created by dario on 16/10/14.
  */
+var questions = [
+    {
+    question: "Who is the Prime Minister of Germany?",
+    choices: ["Georg Bushstein", "Zapatitos", "Angela Merkel"],
+    correct: 2
+    },{
+    question: "What do they eat in Germany",
+    choices: ["Tapas!", "Schnitzel", "Babies' souls"],
+    correct: 2
+    },{
+    question: "What do they drink in Germany",
+    choices: ["Wine", "Water", "Anythingschorle"],
+    correct: 2
+    }
+];
+
