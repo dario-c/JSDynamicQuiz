fa061e3520e55f72f1764d1c11d34758829492ee
dario-c <dariocavanillas@gmail.com>
2014-10-21 12:38:25 +0200

    Reduced Global vars, separated TriviaQuestions into separate file AND ordered functions by appearence

--- quiz.js
+++ quiz.js
@@ -1,52 +1,87 @@
-var usersAnswers = [];
-var currentAnswer;
-var level = 0;
-var nextActivated = false;
+var gameStats = {
+    usersAnswers: [],
+    currentAnswer: undefined,
+    level: 0,
+    correct: 0
+};
+
 var startButton = document.getElementById('start');
 var nextButton = document.createElement("button");
-var correct = 0;
 
-startButton.addEventListener("click", function(){
+function startGame(){
     var parent = this.parentNode;
     parent.removeChild(this);
     var div = composeQuestion();
     parent.insertBefore( div, document.scripts[0] );
-}, false);
+}
 
+function composeQuestion(){
+    var qu = Questions;
+    var questionBox = document.createElement("div");
+    questionBox.id = "question";
+    qu.insertTitleInto(questionBox, gameStats.level);
+    qu.insertChoicesInto(questionBox, gameStats.level);
+    return questionBox;
+}
 
+function listenForSelection(here){
+    here.addEventListener("click", function(event){
+        if(event.target.name === "choices"){
+            checkValue(this);
+            if(!gameStats.nextActivated){ addNextButton(); }
+        }
+    }, false)}
 
-var triviaQuestions = [
-    {
-        question: "Who is the Prime Minister of Germany?",
-        choices: ["Georg Bushstein", "Zapatitos", "Angela Merkel"],
-        correct: 2
-    },{
-        question: "What do they eat in Germany",
-        choices: ["Tapas!", "Schnitzel", "Babies' souls"],
-        correct: 1
-    },{
-        question: "What do they drink in Germany",
-        choices: ["Wine", "Water", "Anythingschorle"],
-        correct: 2
+function addNextButton(){
+    var questionBox = document.getElementById("question").parentNode;
+    questionBox.appendChild(nextButton);
+    gameStats.nextActivated = true;
+}
+
+function refreshStats(){
+    gameStats.usersAnswers[gameStats.level] = gameStats.currentAnswer;
+    gameStats.currentAnswer = undefined;
+    gameStats.nextActivated = false;
+    console.log(gameStats.usersAnswers);
+    gameStats.level++;
+}
+
+function checkValue(here){
+    var allChoices = here.getElementsByTagName("input");
+    for(var i = 0; i < allChoices.length; i++){
+        if (allChoices[i].checked){
+            gameStats.currentAnswer = allChoices[i].value;
+        }
     }
-];
+}
 
+function composeResult(){
+    var resultBox = document.createElement("div");
+    resultBox.id = "result";
+    resultBox.appendChild(document.createTextNode("Success! You have finished the test with: "+ gameStats.correct));
+    return resultBox;
+}
 
+function checkScore(){
+    for(var i = 0; i < gameStats.usersAnswers.length; i++){
+        if(gameStats.usersAnswers[i] == triviaQuestions[i].correct){
+            gameStats.correct++;
+            }
+    }
+}
 
-if(level < triviaQuestions.length) {
+startButton.addEventListener("click", startGame, false);
 
+if(gameStats.level < triviaQuestions.length) {
     listenForSelection(document);
-
     nextButton.appendChild(document.createTextNode(" Next "));
-
     nextButton.addEventListener("click", function () {
         refreshStats();
-
         var parent = this.parentNode;
         parent.removeChild(this);
         parent.removeChild(document.getElementById('question'));
 
-        if(level < triviaQuestions.length){
+        if(gameStats.level < triviaQuestions.length){
             parent.insertBefore(composeQuestion(), document.scripts[0]);
         } else {
             checkScore();
@@ -54,79 +89,3 @@ if(level < triviaQuestions.length) {
         }
     }, false);
 }
-
-
-function refreshStats(){
-    usersAnswers[level] = currentAnswer;
-    currentAnswer = undefined;
-    nextActivated = false;
-    console.log(usersAnswers);
-    level++;
-}
-
-
-function composeQuestion(){
-    var qu = Questions;
-    var questionBox = document.createElement("div");
-    questionBox.id = "question";
-    qu.insertTitleInto(questionBox, level);
-    qu.insertChoicesInto(questionBox, level);
-    return questionBox;
-}
-
-
-function composeResult(){
-    var resultBox = document.createElement("div");
-    resultBox.id = "result";
-    resultBox.appendChild(document.createTextNode("Success! You have finished the test with: "+correct));
-    return resultBox;
-}
-
-function checkScore(){
-    for(var i = 0; i < usersAnswers.length; i++){
-
-        console.log("i ="+i+"    user="+usersAnswers[i] +" Answer= "+ triviaQuestions[i].correct);
-        if(usersAnswers[i] == triviaQuestions[i].correct){
-            correct++;
-            console.log(correct)}
-    }
-
-}
-
-function listenForSelection(question){
-    question.addEventListener("click", function(event){
-        if(event.target.name === "choices"){
-            checkValue(this);
-            if(!nextActivated){ addNextButton(); }
-        }
-    }, false)}
-
-function checkValue(it){
-    var allChoices = it.getElementsByTagName("input");
-    for(var i = 0; i < allChoices.length; i++){
-        if (allChoices[i].checked){
-            currentAnswer = allChoices[i].value;
-        }
-    }
-}
-
-function addNextButton(){
-    var questionBox = document.getElementById("question").parentNode;
-    questionBox.appendChild(nextButton);
-    nextActivated = true;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ triviaQuestions.js
@@ -0,0 +1,15 @@
+var triviaQuestions = [
+    {
+        question: "Who is the Prime Minister of Germany?",
+        choices: ["Georg Bushstein", "Zapatitos", "Angela Merkel"],
+        correct: 2
+    },{
+        question: "What do they eat in Germany",
+        choices: ["Tapas!", "Schnitzel", "Babies' souls"],
+        correct: 1
+    },{
+        question: "What do they drink in Germany",
+        choices: ["Wine", "Water", "Anythingschorle"],
+        correct: 2
+    }
+];
